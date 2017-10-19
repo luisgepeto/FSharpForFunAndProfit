8880b779b4e8311f80a074fbdb887f5de3a669fa
luisgepeto <luisgepeto@gmail.com>
2017-10-19 17:42:00 -0500

    added initial examples

---
+++ 20_exhaustive_pattern_matching.fsx
@@ -0,0 +1,67 @@
+﻿open System.Net.WebRequestMethods
+
+let rec movingAverages list = 
+    match list with
+    | [] -> []
+    | x::y::rest ->
+        let avg = (x+y)/2.0
+        avg :: movingAverages (y :: rest)
+    | [_] -> []
+
+//movingAverages [1.0]
+//movingAverages [1.0;2.0]
+//movingAverages [1.0;2.0;3.0]
+
+type Result<'a, 'b> = 
+    | Success of 'a
+    | Failure of 'b
+
+type FileErrorReason = 
+    | FileNotFound of string
+    | UnauthorizedAccess of string * System.Exception
+
+let performActionOnFile action filePath = 
+    try
+        use sr = new System.IO.StreamReader(filePath:string)
+        let result = action sr
+        sr.Close()
+        Success (result)
+    with
+        | :? System.IO.FileNotFoundException as ex -> Failure (FileNotFound filePath)
+        | :? System.Security.SecurityException as ex -> Failure(UnauthorizedAccess(filePath, ex))
+
+
+let middleLayerDo action filePath =
+    let fileResult = performActionOnFile action filePath
+    fileResult
+
+let topLayerDo action filePath = 
+    let fileResult = middleLayerDo action filePath
+    fileResult
+
+//any client accessing the top layer will be forced to match against a failure pattern
+let printFirstLineOfFile filePath = 
+    let fileResult = topLayerDo (fun fs -> fs.ReadLine()) filePath
+    match fileResult with
+    | Success result -> printfn "first line is: '%s'" result
+    | Failure reason -> 
+        match reason with
+        | FileNotFound file -> printfn "file not found : '%s'" file
+        | UnauthorizedAccess (file,  _) -> printfn "file not accessible : '%s'" file
+
+//we must explicitly ignore any failure
+let printLengthOfFile filePath = 
+    let fileResult = topLayerDo (fun fs -> fs.ReadToEnd().Length) filePath
+    match fileResult with
+    | Success result -> printfn "length is :%i" result
+    | Failure _ -> printfn "Something happened"
+
+let writeSomeText filePath someText = 
+    use writer = new System.IO.StreamWriter(filePath:string)
+    writer.WriteLine(someText:string)
+    writer.Close()
+
+let goodFileName = "good.txt"
+let badFileName = "bad.txt"
+
+writeSomeText goodFileName "hello"
---
+++ 21_type_system_correct_code.fsx
@@ -0,0 +1,77 @@
+﻿
+type EmailAddress = EmailAddress of string
+
+let sendEmail (EmailAddress email) = 
+    printfn "sent an email to %s" email
+
+let aliceEmail = EmailAddress "alice@example.com"
+sendEmail aliceEmail
+
+//this wont compile
+sendEmail "bob@example.com"
+
+//printing values is checked on compile time
+let printingExample = 
+   printf "an int %i" 2                        // ok
+   printf "an int %i" 2.0                      // wrong type
+   printf "an int %i" "hello"                  // wrong type
+   printf "an int %i"                          // missing param
+
+   printf "a string %s" "hello"                // ok
+   printf "a string %s" 2                      // wrong type
+   printf "a string %s"                        // missing param
+   printf "a string %s" "he" "lo"              // too many params
+
+   printf "an int %i and string %s" 2 "hello"  // ok
+   printf "an int %i and string %s" "hello" 2  // wrong type
+   printf "an int %i and string %s" 2          // missing param
+
+
+//types can be inferred from printing strings
+let printAsString x = printf "%s"
+let printAnInt x = printf "%i"
+
+//we can use units of measure
+[<Measure>]
+type cm
+
+[<Measure>]
+type inches
+
+[<Measure>]
+type feet = 
+    static member toInches(feet : float<feet>) : float<inches> =
+        feet*12.0<inches/feet>
+
+let meter = 100.0<cm>
+let yard = 3.0<feet>
+//we can convert units of measurement
+let yardInInches = feet.toInches(yard)
+
+//incorrect units dont match!
+yard+meter
+
+[<Measure>]
+type GBP
+[<Measure>]
+type USD
+
+let gbp10 = 10.0<GBP>
+let usd10 = 10.0<USD>
+gbp10+gbp10 //is allowed
+gbp10+usd10 //not allowed
+gbp10 + 1.0 //not allowed no currency!
+gbp10 + 1.0<_> //ANY CURRENCY DANGER!
+
+//On F# we cannot compare two different objects
+open System
+let obj = new Object()
+let ex = new Exception()
+let b = (obj = ex) //CANNOT DO!
+
+//we can also deny comparison on a type
+[<NoEquality;NoComparison>]
+type CustomerAccount = {CustomerAccountId:int}
+let x={CustomerAccountId =1}
+x = x //This is a compile type error!
+x.CustomerAccountId = x.CustomerAccountId // this can easily be performed :)
